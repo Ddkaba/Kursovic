d77757fb0c2fbaf6c6391447891fcba4c108d9fb
Ddkaba <Danimi2506@gmail.com>
2021-04-22 23:55:37 +0300

    Добавьте файлы проекта.

---
+++ Kursovic/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Kursovic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Human = "", Role = "";
+            Check();
+            string SQL = "SELECT idHuman, Role FROM gibddmodern.password WHERE Login = "+textBox1.Text+" and Password = SHA1("+textBox2.Text+");";
+            MySQL(SQL, Human, Role);
+        }
+
+        public void MySQL(string SQL, string Human, string Role)
+        {
+            string str = "server=localhost;user=root;password=2506Russia5002;database=gibddmodern;port=3306";
+            MySqlConnection connection = new MySqlConnection(str);
+
+            try
+            {
+                connection.Open();
+                string sql = SQL;
+                MySqlCommand mySql = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = mySql.ExecuteReader();
+                while(reader.Read())
+                {
+                    Human = reader["idHuman"].ToString();
+                    Role = reader["Role"].ToString();
+                }
+                Check(Human, Role);
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void Check()
+        {
+           if(textBox1.Text == "" || textBox2.Text == "")
+           {
+                if(textBox1.Text == "")
+                {
+                    if(textBox2.Text == "") MessageBox.Show("Не заполнены оба поля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    else MessageBox.Show("Не заполнено поле Login", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else MessageBox.Show("Не заполнено поле Password", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+           }
+        }
+
+        public void Check(string Human, string Role)
+        {
+            if(Human == "") MessageBox.Show("Запись не найдена", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if(Human != "" & Role != "")
+            {
+                Form form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+        }
+    }
+}
---
+++ Kursovic/Form2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Kursovic
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Gray;
+            textBox2.ForeColor = System.Drawing.Color.Gray;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            string SQL = "call gibddmodern.Number_Car('" + textBox1.Text + textBox2.Text +"');";
+            MySQL(SQL, index);
+        }
+
+        public void MySQL(string SQL, int index)
+        {
+            string str = "server=localhost;user=root;password=2506Russia5002;database=gibddmodern;port=3306";
+            MySqlConnection connection = new MySqlConnection(str);
+
+            try
+            {
+                connection.Open();
+                string sql = SQL;
+                MySqlCommand mySql = new MySqlCommand(sql, connection);
+                Reader(mySql,index);
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void Reader(MySqlCommand mySql, int index)
+        {
+            string Sur = "", Name = "", MiddleName = "";
+            MySqlDataReader reader = mySql.ExecuteReader();
+
+            if (index == 0)
+            {
+                while (reader.Read())
+                {
+                    Sur = reader["Surname"].ToString();
+                    Name = reader["Name"].ToString();
+                    MiddleName = reader["MiddleName"].ToString();
+                    textBox4.Text = reader["PassportNumber"].ToString();
+                    textBox5.Text = reader["NameBrand"].ToString();
+                    textBox6.Text = reader["NameModel"].ToString();
+                    textBox7.Text = reader["BodyNumber"].ToString();
+                    textBox8.Text = reader["Color"].ToString();
+                    textBox9.Text = reader["Category"].ToString();
+                    textBox10.Text = reader["Description"].ToString();
+                }
+                textBox3.Text = Sur + " " + Name + " " + MiddleName; 
+            }
+
+
+
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox2.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = System.Drawing.Color.Black;
+        }
+
+    }
+}
